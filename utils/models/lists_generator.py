d43b053612c1a9bf8641e4210c51a8cee60a6641
NavonSahalo <navonsahalo@gmail.com>
2023-02-08 17:53:52 +0200

    Cuda compatibility

--- utils/models/lists_generator.py
+++ utils/models/lists_generator.py
@@ -1,9 +1,11 @@
-import torchvision
+import torchvision.models as models
 import torch.nn as nn
 import torch.optim as optim
 
 def get_models_list():
-    return [('resnet50', torchvision.models.resnet50()), ]
+    return [('Resnet50', models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)),
+            ('VGG-19', models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1)),
+            ('Mobilenet v2', models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V2)),]
 
 def get_losses_list():
     return [('CrossEntropyLoss', nn.CrossEntropyLoss()), ]
