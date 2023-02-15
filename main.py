45c53b4c1ce910ff706789f469eefe003ece3664
Yoel Abecassis <yoe1997@gmail.com>
2023-02-15 16:30:05 +0200

    Logic should be code

--- main.py
+++ main.py
@@ -17,7 +17,17 @@ first_epochs = 1
 full_train_epochs = 3
 num_classes = 2
 classes = ['Female', 'Male']
-best_comb = defaultdict(int)
+best_comb_occurences = defaultdict(int)
+best_comb_percentage = defaultdict(float)
+
+
+def save_conf_matrix(path, y_real, y_pred):
+    cf_matrix = confusion_matrix(y_real, y_pred)
+    df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1), index=[i for i in classes],
+                         columns=[i for i in classes])
+    plt.figure(figsize=(12, 7))
+    sn.heatmap(df_cm, annot=True)
+    plt.savefig(path)
 
 
 def main():
@@ -27,8 +37,10 @@ def main():
     data_sets = get_data_sets_list()
 
     for DS_name, DS_path in data_sets:
-        out_dir = f'./out/{DS_name}'
-        os.makedirs(out_dir, exist_ok=True)
+        print('=================================================================================================')
+        print(f'Starting resarch best model for {DS_name}')
+        print('=================================================================================================')
+
         data = CustomImageDataset(DS_path, device)
         train_dataloader, test_dataloader = data.get_train_and_test_data()
 
@@ -38,7 +50,7 @@ def main():
         optimizers = get_optimizers_list()
         learning_rates = get_learning_rates_list()
         best_model = best_loss = best_optimizer = best_lr = None
-        best_model_state_dict = None
+        best_model_state_dict = {}
         best_accuracy = 0
         best_iter = None
         y_pred, y_real = [], []
@@ -51,9 +63,11 @@ def main():
             loss_name, loss = loss_tuple
             optimizer_name, optimizer = optimizer_tuple
             lr_name, lr = lr_tuple
+            y_pred, y_real = [], []
+
             print('=================================================================================================')
             print(
-                f'Start training with - Model: {model_name}, Loss: {loss_name}, Optimizer: {optimizer_name}, Learning Rate: {lr_name}')
+                f'Running with Model: {model_name}, Loss: {loss_name}, Optimizer: {optimizer_name}, Learning Rate: {lr_name}')
             print('=================================================================================================')
 
             curr_model = init_model(model, model_name, device, num_classes)
@@ -66,8 +80,10 @@ def main():
                            init_optimizer(optimizer, optimizer_name,
                                           curr_model, lr),
                            device)
-                curr_accuracy = test_loop(
+                curr_accuracy, curr_y_pred, curr_y_real = test_loop(
                     test_dataloader, curr_model, loss, device)
+                y_pred.extend(curr_y_pred)
+                y_real.extend(curr_y_real)
                 if curr_accuracy > best_accuracy:
                     best_accuracy = curr_accuracy
                     best_model = (model_name, curr_model)
@@ -76,16 +92,48 @@ def main():
                     best_lr = lr_tuple
                     best_iter = iter
 
-        print('=================================================================================================')
-        print("Resarch best model is done!")
-        print(
-            f'The best model and hyperparameters are - Model: {best_model[0]} Loss: {best_loss[0]}, Optimizer: {best_optimizer[0]}, Learning Rate: {best_lr[0]}')
-        print('=================================================================================================')
+        out_dir = f'./out/{DS_name}/research_best_model/{best_model[0]}'
+        os.makedirs(out_dir, exist_ok=True)
+        save_conf_matrix(
+            f'/{out_dir}/Confusion_Matrix.png', y_real, y_pred)
 
         print('=================================================================================================')
-        print('Starting with full training')
+        print(f'Resarch best model is done for {DS_name}!')
+        print(
+            f'The best model and hyperparameters for {DS_name} are - Model: {best_model[0]} Loss: {best_loss[0]}, Optimizer: {best_optimizer[0]}, Learning Rate: {best_lr[0]}')
         print('=================================================================================================')
-        best_comb[best_iter] += 1
+
+        best_comb_occurences[best_iter] += 1
+        if best_iter not in best_comb_percentage or best_comb_percentage[best_iter] < best_accuracy:
+            best_comb_percentage[best_iter] = best_accuracy
+
+    # Choose the model that performed the best across all datasets
+    best_comb_iter = None
+    most_used = -1
+    best_accuracy = -1
+
+    for iter in best_comb_occurences:
+        if best_comb_occurences[iter] > most_used or (best_comb_occurences[iter] == most_used and best_comb_percentage[iter] > best_accuracy):
+            most_used = best_comb_occurences[iter]
+            best_accuracy = best_comb_percentage[iter]
+            best_comb_iter = iter
+
+    model_tuple, loss_tuple, optimizer_tuple, lr_tuple = best_comb_iter
+    print('=================================================================================================')
+    print(
+        f'The best model and hyperparameters acorss all datasets are Model: {model_tuple[0]}, Loss: {loss_tuple[0]}, Optimizer: {optimizer_tuple[0]}, Learning Rate: {lr_tuple[0]}')
+    print('=================================================================================================')
+
+    print('=================================================================================================')
+    print('Starting full training on each dataset with the best model')
+    print('=================================================================================================')
+
+    for DS_name, DS_path in data_sets:
+
+        data = CustomImageDataset(DS_path, device)
+        train_dataloader, test_dataloader = data.get_train_and_test_data()
+        curr_model = init_model(model, model_name, device, num_classes)
+
         for t in range(full_train_epochs):
             print(f"Epoch {t+1}\n-------------------------------")
             train_loop(train_dataloader,
@@ -95,29 +143,25 @@ def main():
                                       best_model[1], best_lr[1]),
                        device)
             curr_accuracy, curr_y_pred, curr_y_real = test_loop(
-                test_dataloader, best_model[1], best_loss[1], device, True)
+                test_dataloader, best_model[1], best_loss[1], device)
             y_pred.extend(curr_y_pred)
             y_real.extend(curr_y_real)
             if curr_accuracy > best_accuracy:
                 best_accuracy = curr_accuracy
-                # best_model_state_dict = best_model[1].state_dict()
 
-        # for param_tensor in best_model_state_dict:
-        #     print(param_tensor, "\t", best_model_state_dict[param_tensor].size())
+        # best_model_state_dict.append({DS_name, best_model[1].state_dict()})
+
+        out_dir = f'./out/{DS_name}/best_model_performance/'
+        os.makedirs(out_dir, exist_ok=True)
+        save_conf_matrix(
+            f'{out_dir}/Confusion_Matrix.png', y_real, y_pred)
 
         print('=================================================================================================')
-        print("Train best model is done!")
         print(
-            f'Best accuracy reached: {best_accuracy:>0.2f}%, model is saved in best_model_state_dict')
-        ('=================================================================================================')
-        cf_matrix = confusion_matrix(y_real, y_pred)
-        df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix, axis=1), index=[i for i in classes],
-                             columns=[i for i in classes])
-        plt.figure(figsize=(12, 7))
-        sn.heatmap(df_cm, annot=True)
-        plt.savefig(f'{out_dir}/Confusion_Matrix.png')
+            f'Train best model on {DS_name} is done, Best accuracy reached: {best_accuracy:>0.2f}%, model is saved in best_model_state_dict')
+        print('=================================================================================================')
 
-    print("DONE")
+    print("The run is done")
 
 
 if __name__ == '__main__':
--- networks/train.py
+++ networks/train.py
@@ -53,7 +53,7 @@ def train_loop(dataloader, model, loss_fn, optimizer, device):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-def test_loop(dataloader, model, loss_fn, device, final_model=False):
+def test_loop(dataloader, model, loss_fn, device):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
@@ -73,6 +73,4 @@ def test_loop(dataloader, model, loss_fn, device, final_model=False):
     print(
         f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-    if (final_model):
-        return 100*correct, y_pred, y_true
-    return 100*correct
+    return 100*correct, y_pred, y_true
