7a3100a94ef8e4e5d89cb7d037142356e0f8dc39
NavonSahalu <navon.sahalu@intel.com>
2023-03-04 12:41:17 +0200

    adding graphs

--- main.py
+++ main.py
@@ -28,6 +28,19 @@ def save_conf_matrix(path, y_real, y_pred):
     plt.figure(figsize=(12, 7))
     sn.heatmap(df_cm, annot=True)
     plt.savefig(path)
+    plt.clf()
+
+
+def save_performance_graph(train_data, test_data, num_epochs, title, save_path):
+    epochs = np.linspace(1, num_epochs, num_epochs).astype(int)
+    plt.plot(epochs, train_data, label='train')
+    plt.plot(epochs, test_data, label='test')
+    plt.title(title + ' graph')
+    plt.xlabel('Epochs')
+    plt.ylabel(title)
+    plt.legend()
+    plt.savefig(save_path)
+    plt.clf()
 
 
 def main():
@@ -64,7 +77,7 @@ def main():
             optimizer_name, optimizer = optimizer_tuple
             lr_name, lr = lr_tuple
             y_pred, y_real = [], []
-
+            accuracy_list_train, loss_list_train, accuracy_list_test, loss_list_test = [], [], [], []
             print('=================================================================================================')
             print(
                 f'Running with Model: {model_name}, Loss: {loss_name}, Optimizer: {optimizer_name}, Learning Rate: {lr_name}')
@@ -74,14 +87,18 @@ def main():
 
             for t in range(first_epochs):
                 print(f"Epoch {t+1}\n-------------------------------")
-                train_loop(train_dataloader,
-                           curr_model,
-                           loss,
-                           init_optimizer(optimizer, optimizer_name,
-                                          curr_model, lr),
-                           device)
-                curr_accuracy, curr_y_pred, curr_y_real = test_loop(
+                accuracy_train, loss_train = train_loop(train_dataloader,
+                                                        curr_model,
+                                                        loss,
+                                                        init_optimizer(optimizer, optimizer_name,
+                                                                       curr_model, lr),
+                                                        device)
+                accuracy_list_train.append(accuracy_train)
+                loss_list_train.append(loss_train)
+                curr_accuracy, curr_y_pred, curr_y_real, loss_test = test_loop(
                     test_dataloader, curr_model, loss, device)
+                accuracy_list_test.append(curr_accuracy)
+                loss_list_test.append(loss_test)
                 y_pred.extend(curr_y_pred)
                 y_real.extend(curr_y_real)
                 if curr_accuracy > best_accuracy:
@@ -97,6 +114,10 @@ def main():
         save_conf_matrix(
             f'{out_dir}/Confusion_Matrix.png', y_real, y_pred)
 
+        save_performance_graph(accuracy_list_train, accuracy_list_test,
+                               first_epochs, "Accuracy", f'{out_dir}/Accuracy_graph.png')
+        save_performance_graph(loss_list_train, loss_list_test, first_epochs,
+                               "Loss", f'{out_dir}/Loss_graph.png')
         print('=================================================================================================')
         print(f'Resarch best model is done for {DS_name}!')
         print(
@@ -133,17 +154,19 @@ def main():
         data = CustomImageDataset(DS_path, device)
         train_dataloader, test_dataloader = data.get_train_and_test_data()
         curr_model = init_model(model, model_name, device, num_classes)
-
+        accuracy_list_train, loss_list_train, accuracy_list_test, loss_list_test = [], [], [], []
         for t in range(full_train_epochs):
             print(f"Epoch {t+1}\n-------------------------------")
-            train_loop(train_dataloader,
-                       best_model[1],
-                       best_loss[1],
-                       init_optimizer(best_optimizer[1], best_optimizer[0],
-                                      best_model[1], best_lr[1]),
-                       device)
-            curr_accuracy, curr_y_pred, curr_y_real = test_loop(
+            accuracy_train, loss_train = train_loop(train_dataloader,
+                                                    best_model[1],
+                                                    best_loss[1],
+                                                    init_optimizer(best_optimizer[1], best_optimizer[0],
+                                                                   best_model[1], best_lr[1]),
+                                                    device)
+            curr_accuracy, curr_y_pred, curr_y_real, loss_test = test_loop(
                 test_dataloader, best_model[1], best_loss[1], device)
+            accuracy_list_test.append(curr_accuracy)
+            loss_list_test.append(loss_test)
             y_pred.extend(curr_y_pred)
             y_real.extend(curr_y_real)
             if curr_accuracy > best_accuracy:
@@ -156,6 +179,10 @@ def main():
         save_conf_matrix(
             f'{out_dir}/Confusion_Matrix.png', y_real, y_pred)
 
+        save_performance_graph(accuracy_list_train, accuracy_list_test,
+                               first_epochs, "Accuracy", f'{out_dir}/Accuracy_graph.png')
+        save_performance_graph(loss_list_train, loss_list_test, first_epochs,
+                               "Loss", f'{out_dir}/Loss_graph.png')
         print('=================================================================================================')
         print(
             f'Train best model on {DS_name} is done, Best accuracy reached: {best_accuracy:>0.2f}%, model is saved in best_model_state_dict')
--- networks/train.py
+++ networks/train.py
@@ -38,10 +38,14 @@ def init_optimizer(optim, name, model, lr=0.001, momentum=0.9):
 
 def train_loop(dataloader, model, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    correct, train_loss = 0, 0
     for batch, (X, y) in enumerate(dataloader):
         # Compute prediction and loss
         pred = model(X)
         loss = loss_fn(pred, y.to(device))
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        train_loss += loss.item()
 
         # Backpropagation
         optimizer.zero_grad()
@@ -51,6 +55,10 @@ def train_loop(dataloader, model, loss_fn, optimizer, device):
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+    correct /= size
+    train_loss /= num_batches
+    return 100 * correct, train_loss
 
 
 def test_loop(dataloader, model, loss_fn, device):
@@ -73,4 +81,4 @@ def test_loop(dataloader, model, loss_fn, device):
     print(
         f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-    return 100*correct, y_pred, y_true
+    return 100 * correct, y_pred, y_true, test_loss
