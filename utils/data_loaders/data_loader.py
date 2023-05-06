34fea561dbd6b325af370d916e57955760cffa0b
Yoel Abecassis <yoe1997@gmail.com>
2023-05-06 16:50:29 +0300

    Run time augmentations

--- utils/data_loaders/data_loader.py
+++ utils/data_loaders/data_loader.py
@@ -22,6 +22,8 @@ class CustomImageDataset(Dataset):
     def __init__(self, image_source, device, num_classes, use_file=False):
         self.transform = transforms.Compose([
             transforms.ToPILImage(),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomRotation(degrees=15),
             transforms.Resize(size=(224, 224)),
             transforms.ToTensor(),
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
